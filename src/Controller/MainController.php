c4f9c4c2460a9b7ab901d52c8475a4f03a2ef527
craftidev <ivann.codev@gmail.com>
2023-11-06 19:20:56 +0100

    new about us section: boilerplate/controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,11 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', 'main_')]
 class MainController extends AbstractController
 {
-    #[Route('/', 'main_home')]
+    #[Route('', 'home')]
     public function home()
     {
         return $this -> render('main/home.html.twig');
+    }
+
+    #[Route('about_us', 'about_us')]
+    public function about_us()
+    {
+        return $this->render('main/about_us.html.twig');
     }
 }
