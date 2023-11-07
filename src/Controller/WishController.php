6c09fb80401b63d72371b991778a79c2babf950e
craftidev <ivann.codev@gmail.com>
2023-11-07 14:58:52 +0100

    working list/working detail

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/wish', 'wish_')]
 class WishController extends AbstractController
 {
-    #[Route('/list', 'list')]
-    public function list()
+    #[Route('', 'list')]
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('main/wish/list.html.twig');
+        $wishes = $wishRepository -> findBy([], ['dateCreated' => 'DESC']);
+        return $this->render('main/wish/list.html.twig', [
+            'wishes' => $wishes
+        ]);
     }
 
     #[Route('/detail/{id}', 'detail')]
-    public function detail($id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
+        $wish = $wishRepository -> find($id);
+        
         return $this->render('main/wish/detail.html.twig', [
-            'id' => $id
+            'wish' => $wish
         ]);
     }
 }
